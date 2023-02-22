ec1c1472165e45c46ccbe8e0b18f13065ce71de7
David Arevalo <darevalo-2020048@kinal.edu.gt>
2023-02-22 17:12:09 -0600

    Primer conmit

---
+++ models/productos.js
@@ -0,0 +1,16 @@
+const{ Schema, model } = require('mongoose');
+const ProductoSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre del producto es obligatorio']
+    },
+    descripcion: {
+        type: String,
+        required: [true, 'La descripcion es obligatoria']
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    }  
+});
+module.exports = model('Producto',ProductoSchema)
